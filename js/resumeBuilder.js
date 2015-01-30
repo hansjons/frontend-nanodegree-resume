3d7004c6bcd9b8ede385225913aae1c32e58a18b
Hans Pétur Jónsson <hpjonsson@gmail.com>
2015-01-30 17:08:45 +0000

    Added bio

---
+++ js/resumeBuilder.js
@@ -0,0 +1,21 @@
+var bio = {
+    "name": "John Doe",
+    "role": "Web Developer",
+    "contacts": [
+        {
+            "mobile":   "550-555-5555",
+            "email":    "john@example.com",
+            "github":   "johndoe",
+            "twitter":  "@johndoe",
+            "location": "Reykjavik"
+        }
+    ],
+    "welcomeMessage":   "Lorem ipsum dolor sit amet, consectetur adipiscing " +
+    "elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi " +
+    "ut aliquip ex ea commodo consequat.",
+    "skills":   ["awsomeness", "delivering things", "cryogenic sleep",
+        "serving the universe"],
+    "biopipic": "/images/fry.jpg",
+    "display": function() {}
+}
