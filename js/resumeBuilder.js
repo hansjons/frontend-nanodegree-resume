8b781d58c5e6d4fffe19302547ba992881461656
Hans Pétur Jónsson <hpjonsson@gmail.com>
2015-02-03 11:45:13 +0000

    Added Author

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,21 +1,32 @@
+/*
+ resumeBuilder.js
+ JSON data for project 2 of Udacity Frontend Developer Nanodegree
+ methods to display the data
+ Author: Hans Pétur Jónsson
+ */
+
 var bio = {
     "name": "John Doe",
     "role": "Web Developer",
     "contacts": [
         {
-            "mobile":   "550-555-5555",
-            "email":    "john@example.com",
-            "github":   "johndoe",
-            "twitter":  "@johndoe",
+            "mobile": "550-555-5555",
+            "email": "john@example.com",
+            "github": "johndoe",
+            "twitter": "@johndoe",
             "location": "Reykjavik"
         }
     ],
-    "welcomeMessage":   "Lorem ipsum dolor sit amet, consectetur adipiscing " +
+    "welcomeMessage": "Lorem ipsum dolor sit amet, consectetur adipiscing " +
     "elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
     "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi " +
     "ut aliquip ex ea commodo consequat.",
-    "skills":   ["awsomeness", "delivering things", "cryogenic sleep",
+    "skills": ["awsomeness", "delivering things", "cryogenic sleep",
         "serving the universe"],
-    "biopipic": "/images/fry.jpg",
-    "display": function() {}
-}
+    "biopipic": "./images/fry.jpg",
+    "display": function () {}
+};
+
+
+
+
